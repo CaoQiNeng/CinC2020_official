73f748cc54e83a19ce0aacac801b8018522d8577
cqn <qineng.cao@protontek.com>
2020-06-16 22:48:30 +0800

    fix

--- data_process/hea_to_label.py
+++ data_process/hea_to_label.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from include import *
 
-target = 'PhysioNetChallenge2020_Training_PTB'
+target = 'PhysioNetChallenge2020_PTB-XL'
 root_path = DATA_ROOT_PATH + '/CinC2020_V1'
 st_df = pd.read_csv(root_path + '/SNOMED-CT.csv')
 st_df = st_df.set_index('SNOMED_code')
@@ -32,65 +32,65 @@ for f in os.listdir(hea_path):
                 line = line.replace('#Dx: ','')
                 line = line.replace('\n','')
                 labels = line.split(',')
-                l = st_df.loc[int(labels[0])]['Abbreviation']
-                First_label.append(l)
+                # l = st_df.loc[int(labels[0])]['Abbreviation']
+                First_label.append(int(labels[0]))
                 if len(labels) > 1:
-                    l = st_df.loc[int(labels[1])]['Abbreviation']
-                    Second_label.append(l)
+                    # l = st_df.loc[int(labels[1])]['Abbreviation']
+                    Second_label.append(int(labels[1]))
                 else:
                     Second_label.append('')
 
                 if len(labels) > 2:
-                    l = st_df.loc[int(labels[2])]['Abbreviation']
-                    Third_label.append(l)
+                    # l = st_df.loc[int(labels[2])]['Abbreviation']
+                    Third_label.append(int(labels[2]))
                 else:
                     Third_label.append('')
 
                 if len(labels) > 3:
-                    l = st_df.loc[int(labels[3])]['Abbreviation']
-                    Fourth_label.append(l)
+                    # l = st_df.loc[int(labels[3])]['Abbreviation']
+                    Fourth_label.append(int(labels[3]))
                 else:
                     Fourth_label.append('')
 
                 if len(labels) > 4:
-                    l = st_df.loc[int(labels[4])]['Abbreviation']
-                    Fifth_label.append(l)
+                    # l = st_df.loc[int(labels[4])]['Abbreviation']
+                    Fifth_label.append(int(labels[4]))
                 else:
                     Fifth_label.append('')
 
                 if len(labels) > 5:
-                    l = st_df.loc[int(labels[5])]['Abbreviation']
-                    Sixth_label.append(l)
+                    # l = st_df.loc[int(labels[5])]['Abbreviation']
+                    Sixth_label.append(int(labels[5]))
                 else:
                     Sixth_label.append('')
 
                 if len(labels) > 6:
-                    l = st_df.loc[int(labels[6])]['Abbreviation']
-                    Seventh_label.append(l)
+                    # l = st_df.loc[int(labels[6])]['Abbreviation']
+                    Seventh_label.append(int(labels[6]))
                 else:
                     Seventh_label.append('')
 
                 if len(labels) > 7:
-                    l = st_df.loc[int(labels[7])]['Abbreviation']
-                    Eighth_label.append(l)
+                    # l = st_df.loc[int(labels[7])]['Abbreviation']
+                    Eighth_label.append(int(labels[7]))
                 else:
                     Eighth_label.append('')
 
                 if len(labels) > 8:
-                    l = st_df.loc[int(labels[8])]['Abbreviation']
-                    Ninth_label.append(l)
+                    # l = st_df.loc[int(labels[8])]['Abbreviation']
+                    Ninth_label.append(int(labels[8]))
                 else:
                     Ninth_label.append('')
 
                 if len(labels) > 9:
-                    l = st_df.loc[int(labels[9])]['Abbreviation']
-                    Tenth_label.append(l)
+                    # l = st_df.loc[int(labels[9])]['Abbreviation']
+                    Tenth_label.append(int(labels[9]))
                 else:
                     Tenth_label.append('')
 
                 if len(labels) > 10:
-                    l = st_df.loc[int(labels[10])]['Abbreviation']
-                    Eleventh_label.append(l)
+                    # l = st_df.loc[int(labels[10])]['Abbreviation']
+                    Eleventh_label.append(int(labels[10]))
                 else:
                     Eleventh_label.append('')
 
@@ -107,6 +107,6 @@ df = pd.DataFrame(zip(Recording, First_label, Second_label, Third_label, Fourth_
                            'Fifth_label', 'Sixth_label', 'Seventh_label', 'Eighth_label', 'Ninth_label', 'Tenth_label', 'Eleventh_label', 'data_len'])
 
 df = df.sort_values(by = 'Recording')
-df.to_csv(root_path + '/' + target + '_label.csv', index=False)
+df.to_csv(root_path + '/' + target + '_label_SNOMED.csv', index=False)
 
 print(len_l)
