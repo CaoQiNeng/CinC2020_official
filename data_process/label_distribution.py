fc69957b327d78e021a07acedaf63fdd3c3d4427
cqn <qineng.cao@protontek.com>
2020-06-17 17:52:40 +0800

    rbbb

--- data_process/label_distribution.py
+++ data_process/label_distribution.py
@@ -1,32 +1,36 @@
 from include import *
 
 def do_label_stat(df):
-    class_map = ['AF', 'I-AVB', 'LBBB', 'Normal', 'PAC', 'PVC', 'RBBB', 'STD', 'STE']
+    # ['AF', 'I-AVB', 'LBBB', 'Normal', 'PAC', 'PVC', 'RBBB', 'STD', 'STE']
+    class_map = [164889003, 270492004, 164909002, 426783006, 284470004,  164884008, 59118001, 429622005, 164931005]
     class_map_num = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     df = df.replace(math.nan, 0, regex=True)
 
     for i in range(len(df)) :
         d = df.iloc[i]
-        print(d)
-        d1 = d['First_label']
-        d2 = d['Second_label']
-        d3 = d['Third_label']
-
-        d1_index = class_map.index(d1)
-        class_map_num[d1_index] = class_map_num[d1_index] + 1
-        if not d2 == 0 :
-            d2_index = class_map.index(d2)
-            class_map_num[d2_index] = class_map_num[d2_index] + 1
-        if not d3 == 0 :
-            d3_index = class_map.index(d3)
-            class_map_num[d3_index] = class_map_num[d3_index] + 1
+        for j in range(len(d)) :
+            if d[j] in class_map:
+                d_index = class_map.index(d[j])
+                class_map_num[d_index] = class_map_num[d_index] + 1
+        # d1 = d['First_label']
+        # d2 = d['Second_label']
+        # d3 = d['Third_label']
+        #
+        # d1_index = class_map.index(d1)
+        # class_map_num[d1_index] = class_map_num[d1_index] + 1
+        # if not d2 == 0 :
+        #     d2_index = class_map.index(d2)
+        #     class_map_num[d2_index] = class_map_num[d2_index] + 1
+        # if not d3 == 0 :
+        #     d3_index = class_map.index(d3)
+        #     class_map_num[d3_index] = class_map_num[d3_index] + 1
 
 
     return class_map_num
 
 # ['AF', 'I-AVB', 'LBBB', 'Normal', 'PAC', 'PVC', 'RBBB', 'STD', 'STE']
-stat_df = pd.DataFrame( columns = [164889003, 270492004, 164909002, 426783006, 59118001, 284470004,  164884008,  429622005, 164931005])
-target = 'PhysioNetChallenge2020_PTB-XL_label_SNOMED'
+stat_df = pd.DataFrame(columns = [164889003, 270492004, 164909002, 426783006, 284470004, 164884008, 59118001, 429622005, 164931005])
+target = 'SNOMED_PhysioNetChallenge2020_PTB-XL_label'
 csv_path = DATA_ROOT_PATH + '/CinC2020_V1/' + target + '.csv'
 overall_df = pd.read_csv(csv_path)
 overall_label_stat = do_label_stat(overall_df)
@@ -41,5 +45,5 @@ stat_df.loc['overall'] = overall_label_stat
 #     label_stat = do_label_stat(df)
 #     stat_df.loc['test-a%d_1-687.npy'%i] = label_stat
 
-stat_df.to_csv(DATA_ROOT_PATH + '/CinC2020_V1/Label_Distri' + target + '.csv')
+stat_df.to_csv(DATA_ROOT_PATH + '/CinC2020_V1/Label_Distri_' + target + '.csv')
 
