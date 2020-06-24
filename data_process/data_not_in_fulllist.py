9869f223e07a18a8a82f04dcd06b003b54fd71da
Can <397637675@qq.com>
2020-06-24 09:28:59 +0800

    fix

---
+++ data_process/data_not_in_fulllist.py
@@ -0,0 +1,30 @@
+import os
+import pandas as pd
+from include import *
+
+target = 'PhysioNetChallenge2020_Training_E'
+root_path = DATA_ROOT_PATH + '/CinC2020_V1'
+st_df = pd.read_csv(root_path + '/SNOMED-CT.csv')
+st_df = st_df['SNOMED_code']
+st_df = st_df.tolist()
+
+hea_path = root_path + '/' + target
+f_list = []
+for f in os.listdir(hea_path):
+    if f.lower().endswith('hea'):
+        file = open(hea_path + "/" + f)
+        print(f)
+        for line in file:
+            if line.startswith("#Dx:"):
+                line = line.replace('#Dx: ','')
+                line = line.replace('\n','')
+                labels = line.split(',')
+                # l = st_df.loc[int(labels[0])]['Abbreviation']
+
+                for l in labels:
+                    if int(l) not in st_df:
+                        f_list.append(f.replace('.hea',''))
+
+df = pd.DataFrame(zip(f_list), columns=['f_list'])
+
+df.to_csv(root_path + '/' + target + '_data_not_in_fulllist.csv', index=False)
